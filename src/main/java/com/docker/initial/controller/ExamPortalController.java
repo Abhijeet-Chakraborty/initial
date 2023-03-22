f8abe2e8b6a76c0ddf06603359f5679819afba1e
Abhijeet <abhijeet.chkrbrt7@gmail.com>
2023-03-22 08:23:09 +0530

    Initial commit

---
+++ src/main/java/com/docker/initial/controller/ExamPortalController.java
@@ -0,0 +1,18 @@
+package com.docker.initial.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExamPortalController {
+
+    @GetMapping(value = "v1/api/getMessage")
+    private ResponseEntity getMessage(@RequestParam String user) {
+        return new ResponseEntity(String.format("Hi User %s!", user), HttpStatus.OK);
+    }
+
+}
