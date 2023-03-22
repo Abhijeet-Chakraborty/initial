835a3b99f29796e3b4d5e3c6b23cd2935ceadfa0
Abhijeet <abhijeet.chkrbrt7@gmail.com>
2023-03-22 08:29:46 +0530

    Test case added

--- src/test/java/com/docker/initial/InitialApplicationTests.java
+++ src/test/java/com/docker/initial/InitialApplicationTests.java
@@ -3,11 +3,14 @@ package com.docker.initial;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class InitialApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertEquals(true, true);
 	}
 
 }
