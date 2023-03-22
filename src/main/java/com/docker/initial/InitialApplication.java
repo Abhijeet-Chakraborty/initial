4e0bf06c9c3df1f141f672a649b70fd62164dabc
Abhijeet <abhijeet.chkrbrt7@gmail.com>
2023-03-22 09:07:35 +0530

    Test case added

--- src/main/java/com/docker/initial/InitialApplication.java
+++ src/main/java/com/docker/initial/InitialApplication.java
@@ -1,12 +1,16 @@
 package com.docker.initial;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class InitialApplication {
+	public static Logger LOGGER = LoggerFactory.getLogger(InitialApplication.class);
 
 	public static void main(String[] args) {
+		LOGGER.info("Application Started.......");
 		SpringApplication.run(InitialApplication.class, args);
 	}
 
--- src/test/java/com/docker/initial/InitialApplicationTests.java
+++ src/test/java/com/docker/initial/InitialApplicationTests.java
@@ -1,6 +1,8 @@
 package com.docker.initial;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,8 +10,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 @SpringBootTest
 class InitialApplicationTests {
 
+	public static Logger LOGGER = LoggerFactory.getLogger(InitialApplication.class);
 	@Test
 	void contextLoads() {
+		LOGGER.info("Test Started.......");
 		assertEquals(true, true);
 	}
 
