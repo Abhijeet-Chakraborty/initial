cfbf92e9d4d59ef01242a1be12e295c5a4b9146d
Abhijeet <abhijeet.chkrbrt7@gmail.com>
2023-03-29 00:52:42 +0530

    Updating code for exam portal project

--- src/main/java/com/docker/initial/InitialApplication.java
+++ src/main/java/com/docker/initial/InitialApplication.java
@@ -1,16 +1,14 @@
 package com.docker.initial;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@Slf4j
 @SpringBootApplication
 public class InitialApplication {
-	public static Logger LOGGER = LoggerFactory.getLogger(InitialApplication.class);
-
 	public static void main(String[] args) {
-		LOGGER.info("Application Started.......");
 		SpringApplication.run(InitialApplication.class, args);
 	}
+
 }
